c2522a8d3e1e7409a6da9c711ea403e2400d1ee9
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2023-10-10 17:28:00 +0000

    Restricted Access to Feed and Discover

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
 
+  def feed
+    if @user != current_user
+      redirect_to root_path, alert: "You are not authorized to access this page."
+    else
+      @posts = @user.feed
+    end
+  end
+
+  def discover
+    if @user != current_user
+      redirect_to root_path, alert: "You are not authorized to access this page."
+    else
+      @posts = @user.discover
+    end
+  end
+
   private
 
     def set_user
